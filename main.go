e1404bb8957856103fde148a5fa2c78908840686
Timofey Koolin <rekby@ydb.tech>
2024-08-31 19:34:15 +0300

    multithread benchmark

--- main.go
+++ main.go
@@ -1,10 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	fmt.Println(time.Millisecond * 100 * 1000)
 }
--- mem_find_benchmark_test.go
+++ mem_find_benchmark_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"math/rand/v2"
 	"slices"
+	"sync"
+	"sync/atomic"
 	"testing"
 )
 
@@ -10,11 +12,20 @@ func BenchmarkFind(b *testing.B) {
 	token := genString(rand.New(rand.NewPCG(333, 524)), 30, 30)
 	users := generateUsers(123, 1000000)
 	b.ResetTimer()
-	var res int
-	for range b.N {
-		res += slices.IndexFunc(users, func(user User) bool {
-			return user.Token == token
-		})
+	var res atomic.Int64
+	var wg sync.WaitGroup
+	for range 10 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for range b.N {
+				res.Add(int64(slices.IndexFunc(users, func(user User) bool {
+					return user.Token == token
+				})))
+			}
+		}()
 	}
-	b.Log(res)
+	wg.Wait()
+	b.Log(res.Load())
 }
